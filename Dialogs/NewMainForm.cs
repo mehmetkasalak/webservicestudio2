252bef5f34cfbffe3830ce2967cbbcaca5c720e7
adam.lesien <adam.lesien@gmail.com>
2012-02-09 08:28:02 +0000

    bypassCertificateValidation patch by Herbert Solórzano, thanks

--- Dialogs/NewMainForm.cs
+++ Dialogs/NewMainForm.cs
@@ -250,6 +250,11 @@ namespace WebServiceStudio.Dialogs
             treeOutput.Nodes.Clear();
             propInput.SelectedObject = null;
             propOutput.SelectedObject = null;
+            if (Configuration.MasterConfig.WsdlSettings.BypassCertificateValidation == true)
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate(Object sender1, System.Security.Cryptography.X509Certificates.X509Certificate certificate, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+                { return true; };
+            }
         }
 
         private void ShowMessageInternal(object sender, MessageType status, string message)
